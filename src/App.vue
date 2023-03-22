44036dd7673a4b36cea601c8dba511244304a6f1
Umang Patel <patelumang858@gmail.com>
2023-03-22 12:46:02 -0400

    First commit: Page setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="iPhone Photography School" class="logo" src="./assets/images/ips-logo.svg" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
